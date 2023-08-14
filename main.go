72f8a20a1027488a3524ba91b92f18d8d67117c0
Milton <milton.lima@outlook.com>
2023-08-14 19:24:23 -0300

    first commit :hatched_chick:

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"lookr/cmd"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{Use: "lookr"}
+	rootCmd.AddCommand(
+		cmd.AcmCmd,
+		cmd.AuroraCmd,
+		cmd.CloudFrontCmd,
+		cmd.DynamoDBCmd,
+		cmd.EbsCmd,
+		cmd.EC2Cmd,
+		cmd.ElastiCacheCmd,
+		cmd.ElbCmd,
+		cmd.EksCmd,
+		cmd.IAMCmd,
+		cmd.LambdaCmd,
+		cmd.RdsCmd,
+		cmd.Route53Cmd,
+		cmd.S3Cmd,
+		cmd.SqsCmd,
+	)
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
